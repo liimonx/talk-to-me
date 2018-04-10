2e45e3515b47db78b6a40a46061794ba17a651a9
Limon <limonkhan669@gmail.com>
2018-04-11 00:27:08 +0600

    completed Speech Recognition in speexh function

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,22 @@
+var transcript;
 function speech() {
   window.SpeechRecognition  = window.SpeechRecognition || window.webkitSpeechRecognition;
 
   const recognition = new SpeechRecognition();
   recognition.interimResults = true;
   recognition.lang = 'en-US';
+
+  recognition.addEventListener('result', e => {
+    transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+    console.log(transcript);
+  });
+
+  recognition.addEventListener('end', recognition.start);
+
+  recognition.start();
 }
+speech()
