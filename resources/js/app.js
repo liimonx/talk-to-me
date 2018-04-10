25d8891bd1fbe7853008fac7e5b323d4d9d09ae8
Limon <limonkhan669@gmail.com>
2018-04-11 00:10:46 +0600

    init Speech Recognition

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+function speech() {
+  window.SpeechRecognition  = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+  recognition.lang = 'en-US';
+}
