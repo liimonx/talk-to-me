973917c72215d9ad7c11e52473f1a206268af619
Limon <limonkhan669@gmail.com>
2018-05-12 19:32:03 +0600

    Add some FQA in Switch

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,16 +16,11 @@ recognition.addEventListener('result', e => {
   }
 });
 
-const finalData = (data) => {
-  console.log(data);
-}
 
 document.getElementById('run').addEventListener("click", ()=>{
   recognition.start()
   // recognition.addEventListener('end', recognition.start);
-  
 });
-
 
 
 const msg  = new SpeechSynthesisUtterance();
@@ -34,10 +29,27 @@ let voices = [];
 function populateVoices(){
   voices = this.getVoices();
   msg.voice = voices[5];
-  
 }
-msg.text = "This is so cool";
 
+const finalData = (data) => {
+  
+  switch (data) {
+    case "what is your name":
+      msg.text = "my name is 9";
+      break;
+    case "who made you":
+      msg.text = "my maker limon";
+      break;
+    case "how are you":
+      msg.text = "I am Fine, and you";
+      break;
+  
+    default: msg.text = "sorry, I can understand, what you say";
+      break;
+  }
 
+  console.log(data);
 
+  speechSynthesis.speak(msg);
+}
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
